97e3f38f506cf39fc6116289a28a199a69f41cb1
Chris V <chris@Chriss-MacBook-Air.local>
2021-09-28 13:50:59 -0700

    adding project

---
+++ src/soundBoard.js
@@ -0,0 +1,15 @@
+//code plays sounds and switches Dark/light mode.
+
+function playSound(value) {
+    var input_vol = document.getElementById('volume').value;
+    var vol = input_vol/100
+    const soundEffect = new Audio();
+    soundEffect.src = value;
+    soundEffect.volume = vol;
+    soundEffect.play();
+  }
+
+  function darkLightmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  } 
